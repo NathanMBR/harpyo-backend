913e17c909c19489f2d62686ad7f7eb68788b7f0
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-09 14:07:17 -0300

    chore: add environment variables settings

---
+++ src/settings/env.ts
@@ -0,0 +1,10 @@
+export const NODE_ENV = String(process.env.NODE_ENV);
+export const PORT = Number(process.env.PORT);
+export const SECRET = String(process.env.SECRET);
+export const CONFIRM_ACCOUNT_BASE_URL = String(process.env.CONFIRM_ACCOUNT_BASE_URL);
+
+export const SMTP_HOST = String(process.env.SMTP_HOST);
+export const SMTP_PORT = Number(process.env.SMTP_PORT);
+export const SMTP_USER = String(process.env.SMTP_USER);
+export const SMTP_PASS = String(process.env.SMTP_PASS);
+export const SMTP_FROM = String(process.env.SMTP_FROM);
