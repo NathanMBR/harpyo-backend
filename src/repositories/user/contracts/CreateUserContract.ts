13de3aeeec24bb5f9cc51d6c0497c692a29fb7f1
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-11 18:10:34 -0300

    chore: add password property to create user contract

--- src/repositories/user/contracts/CreateUserContract.ts
+++ src/repositories/user/contracts/CreateUserContract.ts
@@ -9,6 +9,7 @@ export namespace CreateUserContract {
         id: number;
         name: string;
         email: string;
+        password: string;
         createdAt: Date;
         updatedAt: Date;
         deletedAt: Date | null;
