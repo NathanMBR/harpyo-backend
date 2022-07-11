5e67ebeba4fdf794a739e250e28cfe5a6ca46d0f
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-11 17:48:09 -0300

    chore: add read one user by email contract

---
+++ src/repositories/user/contracts/ReadOneUserByEmailContract.ts
@@ -0,0 +1,18 @@
+export namespace ReadOneUserByEmailContract {
+    export interface Request {
+        email: string;
+    }
+
+    export interface Response {
+        id: number;
+        name: string;
+        email: string;
+        createdAt: Date;
+        updatedAt: Date;
+        deletedAt: Date | null;
+    }
+}
+
+export interface ReadOneUserByEmailContract {
+    readOneByEmail: (data: ReadOneUserByEmailContract.Request) => Promise<ReadOneUserByEmailContract.Response | null>
+}
