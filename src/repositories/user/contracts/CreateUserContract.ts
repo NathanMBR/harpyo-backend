dcf2ea5cfa81635e25ab0b3ef2532e9bf49ccf6c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-11 17:44:40 -0300

    chore: add create user contract

---
+++ src/repositories/user/contracts/CreateUserContract.ts
@@ -0,0 +1,20 @@
+export namespace CreateUserContract {
+    export interface Request {
+        name: string;
+        email: string;
+        password: string;
+    }
+
+    export interface Response {
+        id: number;
+        name: string;
+        email: string;
+        createdAt: Date;
+        updatedAt: Date;
+        deletedAt: Date | null;
+    }
+}
+
+export interface CreateUserContract {
+    create: (data: CreateUserContract.Request) => Promise<CreateUserContract.Response>
+}
