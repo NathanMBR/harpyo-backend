d04595dbd79e55a70ef605c9fe1af1735995eb0d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-12 18:20:34 -0300

    chore: add read current user schema

---
+++ src/resources/user/schemas/readCurrentUserDTOSchema.ts
@@ -0,0 +1,17 @@
+import { z as zod } from "zod";
+
+/* eslint-disable camelcase */
+export const readCurrentUserDTOSchema = zod.object(
+    {
+        id: zod
+            .number(
+                {
+                    required_error: "You must provide your user ID",
+                    invalid_type_error: "Your user ID must be a number"
+                }
+            )
+            .positive("Your user ID must be greater than or equal 1")
+            .int("Your user ID must be an integer")
+    }
+);
+/* eslint-enable camelcase */
