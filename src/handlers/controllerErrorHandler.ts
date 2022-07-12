c4eb609d6239425e2696282504ec09efd68b7f06
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-11 21:35:58 -0300

    feat: return zod error

--- src/handlers/controllerErrorHandler.ts
+++ src/handlers/controllerErrorHandler.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { ZodError } from "zod";
 
 import { HttpError } from "@/errors";
 
@@ -9,16 +10,25 @@ export const controllerErrorHandler = (
     if (error instanceof HttpError && error.statusCode !== 500)
         return response.status(error.statusCode).json(
             {
-                statusCode: error.statusCode,
-                message: error.message
+                error: error.message,
+                statusCode: error.statusCode
+            }
+        );
+
+    if (error instanceof ZodError)
+        return response.status(400).json(
+            {
+                error: "Validation error",
+                statusCode: 400,
+                reasons: error.issues.map(issue => issue.message)
             }
         );
 
     console.log(error instanceof HttpError ? error.message : error instanceof Error ? error.stack : error);
     return response.status(500).json(
         {
-            statusCode: 500,
-            message: "Internal server error"
+            error: "Internal server error",
+            statusCode: 500
         }
     );
 };
