34d189bed521987a5cab25b6e2a22fe1280aed21
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-12 19:35:47 -0300

    feat: read current user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
 
 import {
-    CreateUserController
+    CreateUserController,
+    ReadCurrentUserController
 } from "@/controllers/user";
+
+import { AuthenticationMiddleware } from "@/middlewares";
 
 const userRouter = Router();
 
 userRouter.post(
     "/user/create",
     new CreateUserController().handle
+);
+
+userRouter.get(
+    "/user/get",
+    new AuthenticationMiddleware().handle,
+    new ReadCurrentUserController().handle
 );
 
 export { userRouter };
