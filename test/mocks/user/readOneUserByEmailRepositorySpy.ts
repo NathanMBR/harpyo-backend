faf7fe8417122ea45ef70a4b9b4dfa9a307bca79
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-13 20:58:51 -0300

    test: add read one user by email repository mock

---
+++ test/mocks/user/readOneUserByEmailRepositorySpy.ts
@@ -0,0 +1,19 @@
+import { jest } from "@jest/globals";
+
+import { ReadOneUserByEmailContract } from "@/repositories/user";
+
+export const readOneUserByEmailRepositorySpy = jest.fn<ReadOneUserByEmailContract["readOneByEmail"]>(
+    async data => Promise.resolve(
+        data.email === "already.existent.email@test.com"
+            ? {
+                id: 1,
+                name: "Already Existent User",
+                email: "already.existent.email@test.com",
+                password: "already-existent-password",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deletedAt: null
+            }
+            : null
+    )
+);
