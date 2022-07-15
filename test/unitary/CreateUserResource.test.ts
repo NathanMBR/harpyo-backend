ab4c904393dae253b54cbe788ef38cbf76b24a06
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-15 19:22:30 -0300

    test: add all create user unitary tests

--- test/unitary/CreateUserResource.test.ts
+++ test/unitary/CreateUserResource.test.ts
@@ -3,12 +3,14 @@ import {
     it,
     expect
 } from "@jest/globals";
+import { ZodError } from "zod";
 
 import { CreateUserResource } from "@/resources/user";
 import {
     readOneUserByEmailRepositorySpy,
     createUserRepositorySpy
 } from "@test/mocks/user";
+import { BadRequestError } from "@/errors";
 
 const createUserResource = new CreateUserResource(
     {
@@ -23,6 +25,27 @@ describe(
     "Create user unitary tests",
     () => {
         it(
+            "Should successfully create an user",
+            async () => {
+                await expect(
+                    createUserResource.execute(
+                        {
+                            name: "Successfully created user",
+                            email: "successfully.created.user@test.com",
+                            password: "12345678"
+                        }
+                    )
+                )
+                    .resolves
+                    .not
+                    .toThrow();
+
+                expect(readOneUserByEmailRepositorySpy).toHaveBeenCalledTimes(1);
+                expect(createUserRepositorySpy).toHaveBeenCalledTimes(1);
+            }
+        );
+
+        it(
             "Should not create an user with an already existent email",
             async () => {
                 await expect(
@@ -35,9 +58,70 @@ describe(
                     )
                 )
                     .rejects
-                    .toThrow();
+                    .toThrow(BadRequestError);
 
-                expect(readOneUserByEmailRepositorySpy).toHaveBeenCalled();
+                expect(readOneUserByEmailRepositorySpy).toHaveBeenCalledTimes(1);
+                expect(createUserRepositorySpy).not.toHaveBeenCalled();
+            }
+        );
+
+        it(
+            "Should not create an user with a name that has less than 3 characters",
+            async () => {
+                await expect(
+                    createUserResource.execute(
+                        {
+                            name: "AB",
+                            email: "email@test.com",
+                            password: "12345678"
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(ZodError);
+
+                expect(readOneUserByEmailRepositorySpy).not.toHaveBeenCalled();
+                expect(createUserRepositorySpy).not.toHaveBeenCalled();
+            }
+        );
+
+        it(
+            "Should not create an user with an email that's in a bad format",
+            async () => {
+                await expect(
+                    createUserResource.execute(
+                        {
+                            name: "ABC",
+                            email: "bad-format",
+                            password: "12345678"
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(ZodError);
+
+                expect(readOneUserByEmailRepositorySpy).not.toHaveBeenCalled();
+                expect(createUserRepositorySpy).not.toHaveBeenCalled();
+            }
+        );
+
+        it(
+            "Should not create an user with a password that has less than 8 characters",
+            async () => {
+                await expect(
+                    createUserResource.execute(
+                        {
+                            name: "ABCDEFGH",
+                            email: "email@test.com",
+                            password: "1234"
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(ZodError);
+
+                expect(readOneUserByEmailRepositorySpy).not.toHaveBeenCalled();
+                expect(createUserRepositorySpy).not.toHaveBeenCalled();
             }
         );
     }
