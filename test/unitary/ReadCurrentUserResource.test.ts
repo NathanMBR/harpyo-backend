61b5ee1275c922cc466a894551478bee6bdbbaff
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-15 19:35:48 -0300

    test: add read current user unitary tests

---
+++ test/unitary/ReadCurrentUserResource.test.ts
@@ -0,0 +1,107 @@
+import {
+    describe,
+    it,
+    expect
+} from "@jest/globals";
+import { ZodError } from "zod";
+
+import { ReadCurrentUserResource } from "@/resources/user";
+import { readCurrentUserRepositorySpy } from "@test/mocks/user";
+import { NotFoundError } from "@/errors";
+
+const readCurrentUserResource = new ReadCurrentUserResource(
+    {
+        readCurrent: readCurrentUserRepositorySpy
+    }
+);
+
+describe(
+    "Read current user unitary tests",
+    () => {
+        it(
+            "Should successfully read an user",
+            async () => {
+                await expect(
+                    readCurrentUserResource.execute(
+                        {
+                            id: 1
+                        }
+                    )
+                )
+                    .resolves
+                    .not
+                    .toThrow();
+
+                expect(readCurrentUserRepositorySpy).toHaveBeenCalledTimes(1);
+            }
+        );
+
+        it(
+            "Should not read an user with an ID that isn't strictly positive",
+            async () => {
+                await expect(
+                    readCurrentUserResource.execute(
+                        {
+                            id: 0
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(ZodError);
+
+                expect(readCurrentUserRepositorySpy).not.toHaveBeenCalled();
+            }
+        );
+
+        it(
+            "Should not read an user with an ID that isn't an integer",
+            async () => {
+                await expect(
+                    readCurrentUserResource.execute(
+                        {
+                            id: 3.275
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(ZodError);
+
+                expect(readCurrentUserRepositorySpy).not.toHaveBeenCalled();
+            }
+        );
+
+        it(
+            "Should not read an deleted user",
+            async () => {
+                await expect(
+                    readCurrentUserResource.execute(
+                        {
+                            id: 2
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(NotFoundError);
+
+                expect(readCurrentUserRepositorySpy).toHaveBeenCalledTimes(1);
+            }
+        );
+
+        it(
+            "Should not read an inexistent user",
+            async () => {
+                await expect(
+                    readCurrentUserResource.execute(
+                        {
+                            id: 5
+                        }
+                    )
+                )
+                    .rejects
+                    .toThrow(NotFoundError);
+
+                expect(readCurrentUserRepositorySpy).toHaveBeenCalledTimes(1);
+            }
+        );
+    }
+);
