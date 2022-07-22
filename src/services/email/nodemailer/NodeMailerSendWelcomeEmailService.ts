36a6ce79fbe03406957413150b908ecaa7b913e4
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-22 15:47:52 -0300

    chore: add nodemailer send welcome email service

---
+++ src/services/email/nodemailer/NodeMailerSendWelcomeEmailService.ts
@@ -0,0 +1,35 @@
+import path from "path";
+import fs from "fs/promises";
+
+import { SendWelcomeEmailContract } from "../contracts";
+import { nodemailerTransport } from "@/settings";
+import { removeHTMLTagsHelper } from "@/helpers";
+
+export class NodeMailerSendWelcomeEmailService implements SendWelcomeEmailContract {
+    async sendWelcomeEmail(data: SendWelcomeEmailContract.Request) {
+        const welcomeEmailTemplatePath = path.resolve(
+            __dirname,
+            "..",
+            "..",
+            "..",
+            "views",
+            "welcomeEmail.html"
+        );
+
+        const welcomeEmailTemplate = await fs.readFile(welcomeEmailTemplatePath);
+        const welcomeEmail = welcomeEmailTemplate
+            .toString()
+            .replaceAll("{name}", data.to.name)
+            .replaceAll("{confirmAccountURL}", data.confirmAccountURL);
+
+        await nodemailerTransport.sendMail(
+            {
+                from: `${data.from.name} <${data.from.email}>`,
+                to: data.to.email,
+                subject: "Welcome to Harpyo",
+                html: welcomeEmail,
+                text: removeHTMLTagsHelper(welcomeEmail)
+            }
+        );
+    }
+}
