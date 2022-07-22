9fa88711a768b3731d02292ae627cfb14b7380e2
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-07-22 18:13:22 -0300

    chore: add email confirmation property to create user contract

--- src/repositories/user/contracts/CreateUserContract.ts
+++ src/repositories/user/contracts/CreateUserContract.ts
@@ -1,3 +1,15 @@
+interface EmailConfirmation {
+    id: number;
+    userId: number;
+    email: string;
+    token: string;
+    lastRequestedAt: Date;
+    confirmedAt: Date | null;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date | null;
+}
+
 export namespace CreateUserContract {
     export interface Request {
         name: string;
@@ -13,6 +25,8 @@ export namespace CreateUserContract {
         createdAt: Date;
         updatedAt: Date;
         deletedAt: Date | null;
+
+        emailConfirmation: EmailConfirmation;
     }
 }
 
