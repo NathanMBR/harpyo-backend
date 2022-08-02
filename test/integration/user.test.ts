4436b0c03c85c577b6275f7a2d4569433dd1320a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-08-02 20:22:27 -0300

    test: add read current and authenticate user integration tests

--- test/integration/user.test.ts
+++ test/integration/user.test.ts
@@ -20,6 +20,7 @@ import { baseURL } from "@test/settings";
 const prisma = new PrismaClient();
 const request = supertest(app);
 
+let authenticationToken = "";
 beforeAll(
     async () => {
         const password = await hash("password", 16);
@@ -33,13 +34,24 @@ beforeAll(
 
                     emailConfirmations: {
                         create: {
-                            email: "",
+                            email: "user@test.com",
                             confirmedAt: null
                         }
                     }
                 }
             }
         );
+
+        const authenticationResponse = await request
+            .post(`${baseURL}/user/authenticate`)
+            .send(
+                {
+                    email: "user@test.com",
+                    password: "password"
+                }
+            );
+
+        authenticationToken = authenticationResponse.body.token;
     }
 );
 
@@ -74,6 +86,52 @@ describe(
                 expect(response.body).toHaveProperty("createdAt");
                 expect(response.body).toHaveProperty("updatedAt");
                 expect(response.body).toHaveProperty("deletedAt", null);
+            }
+        );
+
+        it(
+            "Should successfully read the current user",
+            async () => {
+                const response = await request
+                    .get(`${baseURL}/user/get`)
+                    .set("Authorization", `Bearer ${authenticationToken}`);
+
+                expect(response.status).toBe(200);
+                expect(response.body).toHaveProperty("id");
+                expect(response.body).toHaveProperty("name", "Test User");
+                expect(response.body).toHaveProperty("email", "user@test.com");
+                expect(response.body).not.toHaveProperty("password");
+                expect(response.body).not.toHaveProperty("emailConfirmation");
+                expect(response.body).toHaveProperty("createdAt");
+                expect(response.body).toHaveProperty("updatedAt");
+                expect(response.body).toHaveProperty("deletedAt", null);
+            }
+        );
+
+        it(
+            "Should successfully authenticate an user",
+            async () => {
+                const userToAuthenticate = {
+                    email: "user@test.com",
+                    password: "password"
+                };
+
+                const response = await request
+                    .post(`${baseURL}/user/authenticate`)
+                    .send(userToAuthenticate);
+
+                expect(response.status).toBe(200);
+                expect(response.body.user).toHaveProperty("id");
+                expect(response.body.user).toHaveProperty("name", "Test User");
+                expect(response.body.user).toHaveProperty("email", userToAuthenticate.email);
+                expect(response.body.user).not.toHaveProperty("password");
+                expect(response.body.user).not.toHaveProperty("emailConfirmation");
+                expect(response.body.user).toHaveProperty("createdAt");
+                expect(response.body.user).toHaveProperty("updatedAt");
+                expect(response.body.user).toHaveProperty("deletedAt", null);
+
+                expect(response.body).toHaveProperty("token");
+                expect(typeof response.body.token).toBe("string");
             }
         );
     }
