0248afc5b04b446effd196606bcc2f4081a30fd7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-08-02 16:20:17 -0300

    chore: add authenticate user schema

---
+++ src/resources/user/schemas/authenticateUserDTOSchema.ts
@@ -0,0 +1,25 @@
+import { z as zod } from "zod";
+
+/* eslint-disable camelcase */
+export const authenticateUserDTOSchema = zod.object(
+    {
+        email: zod
+            .string(
+                {
+                    required_error: "You must provide an e-mail",
+                    invalid_type_error: "Your e-mail must be a text"
+                }
+            )
+            .email("Your e-mail format is invalid")
+            .max(255, "Your e-mail is too long (must have a maximum of 255 characters)"),
+
+        password: zod
+            .string(
+                {
+                    required_error: "You must provide a password",
+                    invalid_type_error: "Your password must be a text"
+                }
+            )
+    }
+);
+/* eslint-enable camelcase */
--- src/resources/user/schemas/index.ts
+++ src/resources/user/schemas/index.ts
@@ -2,3 +2,4 @@ export * from "./createUserDTOSchema";
 export * from "./readCurrentUserDTOSchema";
 export * from "./updateCurrentUserDTOSchema";
 export * from "./inactivateCurrentUserDTOSchema";
+export * from "./authenticateUserDTOSchema";
