8f4fcc5118f645386d3767797e41eda2aeeb1eec
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-08-06 00:39:44 -0300

    chore: add read newest email confirmation by user id resource

---
+++ src/resources/emailConfirmation/readNewestEmailConfirmationByUserIdResource.ts
@@ -0,0 +1,27 @@
+import { z as zod } from "zod";
+
+import { readNewestEmailConfirmationByUserIdDTOSchema } from "./schemas";
+import { ReadNewestEmailConfirmationByUserIdContract } from "@/repositories/emailConfirmation";
+import { NotFoundError } from "@/errors";
+
+type ReadNewestEmailConfirmationByUserIdDTO = zod.infer<typeof readNewestEmailConfirmationByUserIdDTOSchema>;
+
+export class readNewestEmailConfirmationByUserIdResource {
+    constructor(
+        private readonly readNewestEmailConfirmationByUserIdRepository: ReadNewestEmailConfirmationByUserIdContract
+    ) {}
+
+    async execute(dto: ReadNewestEmailConfirmationByUserIdDTO) {
+        const { userId } = readNewestEmailConfirmationByUserIdDTOSchema.parse(dto);
+
+        const emailConfirmation = await this.readNewestEmailConfirmationByUserIdRepository.readNewestByUserId(
+            {
+                userId
+            }
+        );
+        if (!emailConfirmation)
+            throw new NotFoundError("Email confirmation not found");
+
+        return emailConfirmation;
+    }
+}
