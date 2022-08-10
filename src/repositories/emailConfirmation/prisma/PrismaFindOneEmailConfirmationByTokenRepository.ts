bd49955e611e54aea63b9c072b1462e911daf76b
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-08-10 19:02:03 -0300

    chore: add prisma find one email confirmation by token repository

---
+++ src/repositories/emailConfirmation/prisma/PrismaFindOneEmailConfirmationByTokenRepository.ts
@@ -0,0 +1,17 @@
+import { FindOneEmailConfirmationByTokenContract } from "@/repositories/emailConfirmation";
+import { prisma } from "@/database/prisma";
+
+export class PrismaFindOneEmailConfirmationByTokenRepository implements FindOneEmailConfirmationByTokenContract {
+    async findOneByToken(data: FindOneEmailConfirmationByTokenContract.Request) {
+        const emailConfirmation = await prisma.emailConfirmation.findFirst(
+            {
+                where: {
+                    token: data.token,
+                    deletedAt: null
+                }
+            }
+        );
+
+        return emailConfirmation;
+    }
+}
