6f87db433e06a475a05ba48c40f6d8217dba83d6
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-08-10 17:06:35 -0300

    chore: add confirm user schema

---
+++ src/resources/user/schemas/confirmUserDTOSchema.ts
@@ -0,0 +1,16 @@
+import { z as zod } from "zod";
+
+/* eslint-disable camelcase */
+export const confirmUserDTOSchema = zod.object(
+    {
+        token: zod
+            .string(
+                {
+                    required_error: "You must provide a token",
+                    invalid_type_error: "Your token must be a text"
+                }
+            )
+            .uuid("Your token format is invalid")
+    }
+);
+/* eslint-enable camelcase */
--- src/resources/user/schemas/index.ts
+++ src/resources/user/schemas/index.ts
@@ -3,3 +3,4 @@ export * from "./readCurrentUserDTOSchema";
 export * from "./updateCurrentUserDTOSchema";
 export * from "./inactivateCurrentUserDTOSchema";
 export * from "./authenticateUserDTOSchema";
+export * from "./confirmUserDTOSchema";
