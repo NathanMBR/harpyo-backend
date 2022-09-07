9f5e1b1b6080fc39d05a5be1ed18564f6c88d84f
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-07 11:21:28 -0300

    chore: return confirmed at property at read current user mock repository

--- test/mocks/user/readCurrentUserRepositorySpy.ts
+++ test/mocks/user/readCurrentUserRepositorySpy.ts
@@ -8,6 +8,7 @@ const mockedUsers: Array<ReadCurrentUserContract.Response> = [
         name: "Mocked user",
         email: "mocked.user@test.com",
         password: "aiojufdha984i54o4u94dhnjI94U94HOI4h9a4iufg49s84i98u49f8g98h9I84U8HO4I9Ub",
+        confirmedAt: new Date(),
         createdAt: new Date(),
         updatedAt: new Date(),
         deletedAt: null
@@ -17,6 +18,7 @@ const mockedUsers: Array<ReadCurrentUserContract.Response> = [
         name: "Deleted user",
         email: "deleted.user@test.com",
         password: "aiojufdha984i54o4u94dhnjI94U94HOI4h9a4iufg49s84i98u49f8g98h9I84U8HO4I9Uc",
+        confirmedAt: new Date(),
         createdAt: new Date(),
         updatedAt: new Date(),
         deletedAt: new Date()
