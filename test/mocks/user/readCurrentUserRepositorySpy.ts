7845483b6387b007f4527353527548a3338dad9d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-09 13:15:18 -0300

    test: edit read current user repository mock to return hashed passwords

--- test/mocks/user/readCurrentUserRepositorySpy.ts
+++ test/mocks/user/readCurrentUserRepositorySpy.ts
@@ -1,34 +1,44 @@
 import { jest } from "@jest/globals";
+import { hash } from "bcryptjs";
 
 import { ReadCurrentUserContract } from "@/repositories/user";
 
-const mockedUsers: Array<ReadCurrentUserContract.Response> = [
-    {
-        id: 1,
-        name: "Mocked user",
-        email: "mocked.user@test.com",
-        password: "aiojufdha984i54o4u94dhnjI94U94HOI4h9a4iufg49s84i98u49f8g98h9I84U8HO4I9Ub",
-        confirmedAt: new Date(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        deletedAt: null
-    },
-    {
-        id: 2,
-        name: "Deleted user",
-        email: "deleted.user@test.com",
-        password: "aiojufdha984i54o4u94dhnjI94U94HOI4h9a4iufg49s84i98u49f8g98h9I84U8HO4I9Uc",
-        confirmedAt: new Date(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        deletedAt: new Date()
-    }
-];
+const getMockedUser = async (id: number):Promise<ReadCurrentUserContract.Response | null> => {
+    const password = await hash("12345678", 16);
+
+    const mockedUsers = [
+        {
+            id: 1,
+            name: "Mocked user",
+            email: "mocked.user@test.com",
+            password,
+            confirmedAt: new Date(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: null
+        },
+
+        {
+            id: 2,
+            name: "Deleted user",
+            email: "deleted.user@test.com",
+            password,
+            confirmedAt: new Date(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: new Date()
+        }
+    ];
+
+    const user = mockedUsers[id];
+
+    return user || null;
+};
 
 export const readCurrentUserRepositorySpy = jest.fn<ReadCurrentUserContract["readCurrent"]>(
     async data => {
         const userId = data.id - 1;
-        const user = mockedUsers[userId];
+        const user = await getMockedUser(userId);
 
         return user && !user.deletedAt ? user : null;
     }
