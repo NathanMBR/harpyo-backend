15e395cc07f17b24e578a06a0f04b504e4f8d339
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-23 19:26:31 -0300

    feat: gracefully shutdown server

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import {
     NODE_ENV
 } from "@/settings";
 import { healthCheckHelper } from "@/helpers";
-import { serverListeningHandler } from "@/handlers";
+import {
+    serverListeningHandler,
+    serverShutdownHandler
+} from "@/handlers";
 
 const startServer = async () => {
     await healthCheckHelper();
 
-    app.listen(
+    const server = app.listen(
         PORT,
         serverListeningHandler(PORT, NODE_ENV)
+    );
+
+    process.on(
+        "exit",
+        serverShutdownHandler(server)
+    );
+
+    process.on(
+        "SIGINT",
+        serverShutdownHandler(server)
+    );
+
+    process.on(
+        "SIGTERM",
+        serverShutdownHandler(server)
     );
 };
 
