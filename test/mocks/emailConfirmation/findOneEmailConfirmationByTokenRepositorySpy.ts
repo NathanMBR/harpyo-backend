9d6e2de9bc1e97024f8001b86cdd7c4c7d19df76
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-29 19:32:33 -0300

    test: return null at find one email confirmation by token repository mock

--- test/mocks/emailConfirmation/findOneEmailConfirmationByTokenRepositorySpy.ts
+++ test/mocks/emailConfirmation/findOneEmailConfirmationByTokenRepositorySpy.ts
@@ -3,16 +3,18 @@ import { jest } from "@jest/globals";
 import { FindOneEmailConfirmationByTokenContract } from "@/repositories/emailConfirmation";
 
 export const findOneEmailConfirmationByTokenRepositorySpy = jest.fn<FindOneEmailConfirmationByTokenContract["findOneByToken"]>(
-    async data => Promise.resolve(
-        {
-            id: 1,
-            userId: 1,
-            email: "random.user@test.com",
-            token: data.token,
-            confirmedAt: null,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            deletedAt: null
-        }
-    )
+    async data => data.token === "bb6ecb51-30cd-4dc2-9151-aff712ebd8a6"
+        ? Promise.resolve(null)
+        : Promise.resolve(
+            {
+                id: 1,
+                userId: 1,
+                email: "random.user@test.com",
+                token: data.token,
+                confirmedAt: null,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deletedAt: null
+            }
+        )
 );
