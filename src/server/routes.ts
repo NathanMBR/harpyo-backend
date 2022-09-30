aba26e2e9499ad71502b6bcda2e233ee86535dcf
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-30 17:38:51 -0300

    fix: attach folder router to server routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import {
-    userRouter
+    userRouter,
+    folderRouter
 } from "@/routes";
 
 const routes = Router();
 routes.use(userRouter);
+routes.use(folderRouter);
 
 export { routes };
