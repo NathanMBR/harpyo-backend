738b4859ff61d6f46307a4dd413c5f8929c63c20
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-09-30 20:13:01 -0300

    chore: add find all folders schema

---
+++ src/resources/folder/schemas/findAllFoldersDTOSchema.ts
@@ -0,0 +1,69 @@
+import { z as zod } from "zod";
+
+/* eslint-disable camelcase */
+export const findAllFoldersDTOSchema = zod.object(
+    {
+        skip: zod
+            .number(
+                {
+                    required_error: "The skip amount is required",
+                    invalid_type_error: "The skip amount must be a number",
+                    description: "The skip amount"
+                }
+            )
+            .int("The skip amount must be an integer")
+            .positive("The skip amount must be positive"),
+
+        take: zod
+            .number(
+                {
+                    required_error: "The take amount is required",
+                    invalid_type_error: "The take amount must be a number",
+                    description: "The take amount"
+                }
+            )
+            .int("The take amount must be an integer")
+            .positive("The take amount must be positive"),
+
+        orderColumn: zod
+            .string(
+                {
+                    invalid_type_error: "The order column must be a string",
+                    description: "The column to order by"
+                }
+            )
+            .refine(
+                orderColumn => orderColumn === "id" || orderColumn === "updatedAt",
+                {
+                    message: "The order column must be either \"id\" or \"updatedAt\""
+                }
+            )
+            .optional(),
+
+        orderBy: zod
+            .string(
+                {
+                    required_error: "The order direction is required",
+                    invalid_type_error: "The order direction must be a string",
+                    description: "The order direction"
+                }
+            )
+            .refine(
+                orderBy => orderBy === "asc" || orderBy === "desc",
+                {
+                    message: "The order direction must be either \"asc\" or \"desc\""
+                }
+            ),
+
+        search: zod
+            .string(
+                {
+                    required_error: "The search query is required",
+                    invalid_type_error: "The search query must be a string",
+                    description: "The search query"
+                }
+            )
+            .max(255, "The search query is too long (must have a maximum of 255 characters)")
+    }
+);
+/* eslint-enable camelcase */
--- src/resources/folder/schemas/index.ts
+++ src/resources/folder/schemas/index.ts
@@ -1,1 +1,2 @@
 export * from "./createFolderDTOSchema";
+export * from "./findAllFoldersDTOSchema";
