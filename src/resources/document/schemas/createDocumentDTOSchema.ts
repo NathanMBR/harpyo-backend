8817fe0482f8c68515e03e09f2a346ebf138c933
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-04 11:54:01 -0300

    chore: add create document schema

---
+++ src/resources/document/schemas/createDocumentDTOSchema.ts
@@ -0,0 +1,58 @@
+import { z as zod } from "zod";
+
+/* eslint-disable camelcase */
+export const createDocumentDTOSchema = zod.object(
+    {
+        title: zod
+            .string(
+                {
+                    required_error: "You must provide a document title",
+                    invalid_type_error: "The document title must be a text",
+                    description: "The document title"
+                }
+            )
+            .min(1, "The document title is too short (must have a minimum of 1 character)")
+            .max(255, "The document title is too long (must have a maximum of 255 characters)"),
+
+        text: zod
+            .string(
+                {
+                    invalid_type_error: "The document text must be a text",
+                    description: "The document text"
+                }
+            )
+            .nullable(),
+
+        folderId: zod
+            .number(
+                {
+                    invalid_type_error: "The folder ID must be a number",
+                    description: "The folder ID"
+                }
+            )
+            .int("The folder ID must be an integer")
+            .positive("The folder ID must be positive")
+            .nullable(),
+
+        userId: zod
+            .number(
+                {
+                    required_error: "You must provide an user ID",
+                    invalid_type_error: "The user ID must be a number",
+                    description: "The current user ID"
+                }
+            )
+            .int("The user ID must be an integer")
+            .positive("The user ID must be positive"),
+
+        isEncrypted: zod
+            .boolean(
+                {
+                    required_error: "You must set the document as encrypted or not",
+                    invalid_type_error: "The document encryption status must be a boolean",
+                    description: "The document encryption status"
+                }
+            )
+    }
+);
+/* eslint-enable camelcase */
