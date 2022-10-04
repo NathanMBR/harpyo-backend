19b69e8310d080f7a907be79abf10813015cc71c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-04 20:25:07 -0300

    chore: add prisma find all documents repository

---
+++ src/repositories/document/prisma/PrismaFindAllDocumentsRepository.ts
@@ -0,0 +1,53 @@
+import { FindAllDocumentsContract } from "@/repositories/document";
+import { prisma } from "@/database/prisma";
+import { Prisma } from "@prisma/client";
+
+export class PrismaFindAllDocumentsRepository implements FindAllDocumentsContract {
+    async findAll(data: FindAllDocumentsContract.Request) {
+        const orderByArray: Prisma.Enumerable<Prisma.DocumentOrderByWithRelationInput> = [
+            {
+                title: "asc"
+            }
+        ];
+
+        if (data.orderColumn)
+            orderByArray.unshift(
+                {
+                    [data.orderColumn]: data.orderBy
+                }
+            );
+
+        const documents = await prisma.document.findMany(
+            {
+                where: {
+                    user: {
+                        id: data.userId,
+                        deletedAt: null
+                    },
+
+                    OR: [
+                        {
+                            title: {
+                                contains: data.search
+                            }
+                        },
+
+                        {
+                            text: {
+                                contains: data.search
+                            }
+                        }
+                    ],
+
+                    deletedAt: null
+                },
+
+                skip: data.skip,
+                take: data.take,
+                orderBy: orderByArray
+            }
+        );
+
+        return documents;
+    }
+}
--- src/repositories/document/prisma/index.ts
+++ src/repositories/document/prisma/index.ts
@@ -1,1 +1,2 @@
 export * from "./PrismaCreateDocumentRepository";
+export * from "./PrismaFindAllDocumentsRepository";
