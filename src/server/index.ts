4a7e9610f598ef0c261c20414b0ebb735477bce8
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-06 16:34:33 -0300

    chore: use morgan to log http requests

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import { routes } from "./routes";
 
@@ -9,6 +10,7 @@ app.disable("x-powered-by");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(morgan("combined"));
 
 app.use("/api", routes);
 
